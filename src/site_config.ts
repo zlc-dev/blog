02b4340870d819e2e2b237531a536fbf85472a2c
zlc <zlcdev@foxmail.com>
2026-01-12 11:05:30 +0800

    feat: banner background settings

--- src/site_config.ts
+++ src/site_config.ts
@@ -3,11 +3,17 @@ type SiteConfig = {
     base: string;
     cdn: string;
     giscus: {
-        data_repo: string,
-        data_repo_id: string,
-        data_category: string,
-        data_category_id: string
-    }
+        data_repo: string;
+        data_repo_id: string;
+        data_category: string;
+        data_category_id: string;
+    };
+    apperance: {
+        banner_background: {
+            url: string;
+            credit: string;
+        },
+    };
 }
 
 export const SITE_CONFIG = {
@@ -19,5 +25,11 @@ export const SITE_CONFIG = {
         data_repo_id: "R_kgDOQvQ5cA",
         data_category: "Announcements",
         data_category_id: "DIC_kwDOQvQ5cM4C0Qvk",
+    },
+    apperance: {
+        banner_background: {
+            url: 'url("https://cn.bing.com/th?id=OHR.WiltshireDawn_ZH-CN2887906329_1920x1080.jpg")',
+            credit: "© Bing"
+        }
     }
 } as const satisfies SiteConfig;
