33176c1d9a16011f11285f71908d3f338ba8cba9
zlc <zlcdev@foxmail.com>
2026-01-22 02:12:28 +0800

    refactor: remove src/utils/aplayer.ts

--- src/utils/aplayer.ts
+++
@@ -1,9 +0,0 @@
-export type AplayerAudio = {
-    name: string;          // 歌名（必填）
-    artist: string;        // 艺术家（必填）
-    url: string;           // 音频地址（必填，CDN / 本地）
-    cover?: string;        // 封面
-    lrc?: string;          // 歌词（lrc 文件或字符串）
-    theme?: string;        // 单曲主题色
-    type?: 'auto' | 'hls' | 'normal'; // 音频类型
-};
